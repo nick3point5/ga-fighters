9ef93a784278e9313ffe2d6ef7011d52482ac0de
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-01-15 15:58:04 -0600

    modles exported and connected

--- config/database.js
+++ config/database.js
@@ -1,17 +1,23 @@
-const mongoose = require('mongoose')
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
-mongoose.connect(connectionString, { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+const mongoose = require('mongoose');
+const connectionString =
+	process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
+mongoose.connect(connectionString, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
 });
 mongoose.connection.on('connected', () => {
-    console.log(`Mongoose connected`);
+	console.log(`Mongoose connected`);
 });
 mongoose.connection.on('error', (err) => {
-    console.log(`Mongoose connected error `+err);
+	console.log(`Mongoose connected error ` + err);
 });
 mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose disconnected');
+	console.log('Mongoose disconnected');
 });
+
+module.exports = {
+	User: require('../models/User'),
+	Avatar: require('../models/Avatar'),
+};
