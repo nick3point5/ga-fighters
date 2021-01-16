e82868e1d6cbb323f60fe6c41f682c3ed554e77f
kennytrinh <kenny.trinh4444@gmail.com>
2021-01-15 21:41:21 -0600

    stat bunttons

--- config/database.js
+++ config/database.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const connectionString =
 	process.env.MONGODB_URI || 'mongodb+srv://nick3point5:Am8ltK7i3OvzWgtR@cluster0.c2x5q.mongodb.net/test?retryWrites=true&w=majority';
 	
-
 mongoose.connect(connectionString, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
