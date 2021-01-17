47dcc0cd5131119625b9647299bc3b9ad31824a3
kennytrinh <kenny.trinh4444@gmail.com>
2021-01-16 19:25:38 -0600

    uninstall useless apis

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ require('./config/database');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-// const flash = require('connect-flash')
-// const session = require('express-session')
 const indexRoute = require('./controllers/indexRoute.js');
 const avatarRoute = require('./controllers/avatarRoute.js');
 const PORT = 3000;
@@ -17,16 +15,7 @@ app.use((req, res, next) => {
 app.use('/index', express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-// app.use(flash())
-// app.use(function(req, res, next){
-//     res.locals.success = req.flash('success');
-//     res.locals.errors = req.flash('error');
-//     next();
-// });
-// app.use(session({ cookie: { maxAge: 60000 }, 
-// 	secret: 'woot',
-// 	resave: false, 
-// 	saveUninitialized: false}));
+
 // GET AT LOCAL HOST: /
 app.get('/', (req, res) => {
 	res.render('index');
